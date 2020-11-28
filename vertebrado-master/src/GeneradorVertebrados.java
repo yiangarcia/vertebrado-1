e8f82fdd9136393eb4d067528f035a5eabcd1e5e
YianK <YianK@YianK-PC>
2020-11-27 21:19:37 -0500

    Paloma Eliminado

--- vertebrado-master/src/GeneradorVertebrados.java
+++ vertebrado-master/src/GeneradorVertebrados.java
@@ -1,10 +1,10 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados (){
-        Vertebrado[] miVertebrados = new Vertebrado[4];
+        Vertebrado[] miVertebrados = new Vertebrado[3];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero"); 
-        miVertebrados[2] = new Vertebrado("Paloma", "Ave"); 
-        miVertebrados[3] = new Vertebrado("Tiburón", "Pez"); 
+       // miVertebrados[2] = new Vertebrado("Paloma", "Ave"); 
+        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
         return miVertebrados;
     }
